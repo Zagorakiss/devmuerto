31b85a8b750be04414e36c4c62c605a2500fc98a
Zagora <zagorakom@gmail.com>
2016-01-07 17:33:48 +0100

    Update 7.11.2016

--- js/app.js
+++ js/app.js
@@ -12,13 +12,31 @@
 
 
 
+// SCROLLBAR -----------------------------------------------------
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll({cursorwidth:"10px",cursorborderradius:"0px",cursorcolor:"#271900",cursorborder:"1px solid #ffa500",cursoropacitymin:"0.1",hwacceleration:"false",background:"rgba(39,25,0,0.1)",hidecursordelay:"100"});
+    $("#terminal").niceScroll({cursorcolor:"#ffa500",cursorborder:"1px solid #271900",cursoropacitymin:"0.5",hwacceleration:"false"});
+  }
+
+);
+// SCROLLBAR END -------------------------------------------------
+
+
+
+
+
+
+
+
 
 
 
 
 
 // HEADER ---------------------------------------------------
-
 
 $(function() { 
   // Replay button
